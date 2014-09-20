4cd8ac591080b5aa125705719d12cf7e246dc866
Paul Fechtmeister <paulfecht@hotmail.com>
2014-09-20 17:24:22 -0600

    Minified our HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
-    // minifyHTML = require('gulp-minify-html'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 var env,
@@ -85,7 +85,7 @@ gulp.task('connect', function() {
 
 gulp.task('html', function() {
   gulp.src('builds/development/*.html')
-    // .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', minifyHTML()))
     .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
     .pipe(connect.reload())
 });
